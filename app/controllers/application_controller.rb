9fee9a98e5272253296c1c65e8bf73fa76eefc8b
dadamu4 <bessiadamu@gmail.com>
2018-06-29 16:22:40 +0000

    new

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,70 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    return erb :index
+    erb :index
   end
+
+  get '/highschool' do
+    erb :highschool
+  end
+
+  get '/college' do
+    erb :college
+  end
+
+  get '/suicide' do
+    erb :suicide
+  end
+
+  get '/collegelistings' do
+    erb :collegelistings
+  end
+
+  get '/gpalist' do
+    erb :gpalist
+  end
+
+  get '/entertainment' do
+    erb :entertainment
+  end
+
+  post '/movielist' do
+      @choice = params[:genre]
+      erb :movielist
+  end
+
+  post '/booklist' do
+      @choice = params[:genre]
+      erb :booklist
+  end
+
+  post '/songlist' do
+      @choice = params[:genre]
+      erb :songlist
+  end
+
+  get '/memes' do
+    erb :memes
+  end
+
+  post '/collegelistings' do
+      @choice = params[:major]
+      erb :collegelistings
+  end
+
+  post '/gpalist' do
+      @choice = params[:gpa]
+      erb :gpalist
+  end
+
+
+  get '/health' do
+    erb :health
+  end
+
+  post '/healthinformation' do
+    @choice = params[:emotion]
+    erb :healthinformation
+  end
+
 end
